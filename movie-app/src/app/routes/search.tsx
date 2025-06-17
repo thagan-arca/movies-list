501f2612211626600ea59814bfc526152f236d6b
thagan-arca <214645868+thagan-arca@users.noreply.github.com>
2025-06-17 12:36:43 +0100

    Creating the project using Next was overwhelming so I will implement Remix as I have a step-by-step guide for this process. Therefore, movie-remix has been created.

---
+++ movie-app/src/app/routes/search.tsx
@@ -0,0 +1,11 @@
+// import { Outlet } from "react-router-dom";
+
+export default function Search() {
+  return (
+    <main>
+      {/* <Outlet /> */}
+      <h1>TEST</h1>
+      <main></main>
+    </main>
+  );
+}
