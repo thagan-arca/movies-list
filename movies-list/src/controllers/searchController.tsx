91c4aa74de591cecde365e1c347260b55f445cd0
thagan-arca <214645868+thagan-arca@users.noreply.github.com>
2025-06-17 11:20:02 +0100

    react-create-app deprecated so next.js was used: npx create-next-app@latest
    searchController edited to include BEARER_TOKEN and pass GET request to TMDB API.
    movies-list/ will be removed in future commit.

--- movies-list/src/controllers/searchController.tsx
+++ movies-list/src/controllers/searchController.tsx
@@ -1,17 +1,39 @@
-// const asyncHandler = require("express-async-handler");
+// // const asyncHandler = require("express-async-handler");
+// const dotenv = require("dotenv").config();
+// const BEARER_TOKEN = process.env.REACT_APP_BEARER_TOKEN || "No Key";
+// console.log(BEARER_TOKEN);
 
-// let headersList = {
-//  "Accept": "*/*",
-//  "User-Agent": "Thunder Client (https://www.thunderclient.com)"
-// }
+// // let headersList = {
+// //  "Accept": "*/*",
+// //  "User-Agent": "Thunder Client (https://www.thunderclient.com)"
+// // }
 
-// //@desc Get movie
-// //@route GET /movies/:movieId
+// // //@desc Get movie
+// // //@route GET /movies/:movieId
+// // //@access private
+// // let response = await fetch("https://www.themoviedb.org/search?language=en-GB&query=saw", {
+// //   method: "GET",
+// //   headers: headersList
+// // });
+
+// // let data = await response.text();
+// // console.log(data);
+
+// //@desc Get movies
+// //@route GET /movies/
 // //@access private
-// let response = await fetch("https://www.themoviedb.org/search?language=en-GB&query=saw", { 
+// const url = "https://api.themoviedb.org/3/discover/movie";
+// const options = {
 //   method: "GET",
-//   headers: headersList
-// });
+//   headers: {
+//     accept: "application/json",
+//     Authorization: `Bearer ${BEARER_TOKEN}`,
+//   },
+// };
 
-// let data = await response.text();
-// console.log(data);
+// console.log(options);
+
+// fetch(url, options)
+//   .then((res) => res.json())
+//   .then((json) => console.log(json))
+//   .catch((err) => console.error(err));
