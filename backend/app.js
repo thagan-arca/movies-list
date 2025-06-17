c1a07924aa31387faa7abd08c0c88026b257545c
thagan-arca <214645868+thagan-arca@users.noreply.github.com>
2025-06-17 15:33:14 +0100

    Fonts added (will be changed to a less whimsical one in the future. Programming of backend functionality to begin.

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,5 @@
 const express = require('express');
-// import { dotenv } from dotenv.config();
 const app = express();
-// // const dotenv = require("dotenv").config();
 
 app.use((req, res) => {
     res.status(200).send('Backend to retrieve API data and implement middleware for caching and rate limiting.');
@@ -9,6 +7,27 @@ app.use((req, res) => {
 // Start the server
 const PORT = process.env.REACT_APP_PORT || 8080;
 // const PORT = 8080;
+
+app.use((req, res, next) => {
+  // Attach CORS headers
+  // Required when using a detached backend (that runs on a different domain)
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+//@desc Get movies
+//@route GET /movies/
+//@access private
+const url = "https://api.themoviedb.org/3/discover/movie";
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${BEARER_TOKEN}`,
+  },
+};
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
